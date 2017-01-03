362c83dccd4c803debf593e1550fc345f7851861
Most Wanted <bmwant@gmail.com>
2017-01-03 07:48:09 +0200

    Remove unused files

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 TESTING = False
 SECRET_KEY = 'some-radnom-conbimation'
 DEBUG = True
-HOST = '0.0.0.0'
+HOST = '127.0.0.1'
 PORT = 3234
-REDIS_HOST = '10.0.3.3'
+REDIS_HOST = '127.0.0.1'
 REDIS_PORT = 6379
-MONGO_URI = 'mongodb://10.0.3.3:27017'
+MONGO_URI = 'mongodb://127.0.0.1:27017'
